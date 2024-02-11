914d9ef5e9eb7fe2e675ebefed2a6174b9cae9ee
Sushant Kumar Singh <88628113+Sushantkumarsingh01@users.noreply.github.com>
2024-02-11 21:13:26 +0530

    Delete Case Study/Code directory

--- Case Study/Code/DatabaseConnectionException.py
+++
@@ -1,6 +0,0 @@
-# exception/DatabaseConnectionException.py
-
-class DatabaseConnectionException(Exception):
-    def __init__(self, message="Database connection issue"):
-        self.message = message
-        super().__init__(self.message)
--- Case Study/Code/InvalidInputException.py
+++
@@ -1,6 +0,0 @@
-# exception/InvalidInputException.py
-
-class InvalidInputException(Exception):
-    def __init__(self, message="Invalid input data"):
-        self.message = message
-        super().__init__(self.message)
